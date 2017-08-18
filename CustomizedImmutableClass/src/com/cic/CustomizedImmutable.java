e635c6d6e0e1ced88212a518d0500ad63c363f82
Shreerang Hartalkar <write2shreerang@gmail.com>
2017-08-19 04:52:38 +0530

    Initial commit

---
+++ CustomizedImmutableClass/src/com/cic/CustomizedImmutable.java
@@ -0,0 +1,35 @@
+package com.cic;
+
+final class CustomImmutable
+{
+	private int i;
+
+	 CustomImmutable(int i) {
+		this.i = i;
+	}
+	 
+	 public CustomImmutable modify(int i)
+	 {
+		 if(this.i==i)
+		 {
+			 return this;
+		 }
+		 else
+		 {
+			 return new CustomImmutable(i); //create CustomImmutable obj with that value
+		 }
+			 
+	 }
+	 public static void main(String[] args) {
+		CustomImmutable ci = new CustomImmutable(10);
+		CustomImmutable ci1 = ci.modify(100);
+		CustomImmutable ci2 = ci.modify(10);
+		System.out.println(ci==ci1);//false
+		System.out.println(ci==ci2);//true
+		System.out.println("This is our Immutable class");
+		
+	}
+	
+
+}
+
